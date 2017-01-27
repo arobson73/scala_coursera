4a677b7ed7a02a22693fe57a2b7704a611353006
AR <arobson73@yahoo.co.uk>
2017-01-27 11:11:14 +0000

    Create getFileExample.scala

---
+++ getFileExample.scala
@@ -0,0 +1,30 @@
+package object forcomp {
+  val dictionaryPath = List("forcomp", "linuxwords.txt")
+
+  def loadDictionary = {
+    val wordstream = Option {
+      getClass.getResourceAsStream(dictionaryPath.mkString("/"))
+    } orElse {
+      common.resourceAsStreamFromSrc(dictionaryPath)
+    } getOrElse {
+      sys.error("Could not load word list, dictionary file not found")
+    }
+    try {
+      val s = io.Source.fromInputStream(wordstream)
+      s.getLines.toList
+    } catch {
+      case e: Exception =>
+        println("Could not load word list: " + e)
+        throw e
+    } finally {
+      wordstream.close()
+    }
+  }
+
+}
+
+//directory structure was like this resources/forcomp/linuxwords.txt
+//scala{common,forcomp}
+//scala/forcomp/package.scala (this file)
+//scala/forcomp/anagrams.scala (this file has call like this (  val dictionary: List[Word] = loadDictionary
+)
